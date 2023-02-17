ace8e60de099f85bf94a268b49a6677649a7c977
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2023-02-17 06:47:21 +0100

    First half of the tutorial completed

---
+++ react-weather-app/src/Api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e00606ec43msh273823f3e2720dfp1ad87djsn2b6bc2a4f6c0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "0cbb790b51f92cdfda7177c14bf33d2e";
---
+++ react-weather-app/src/components/current-weather/CurrentWeather.js
@@ -0,0 +1,41 @@
+import './CurrentWeather.css'
+
+const CurrentWeather = ({data}) => {
+    return (
+        <div className='weather'>
+            <div className='top'>
+                <div>
+                    <p className='city'>{data.city}</p>
+                    <p className='weather-description'>{data.weather[0].description}</p>
+                </div>
+                <img alt='weather' className='weather-icon' src={`icons/${data.weather[0].icon}.png`} />
+            </div>
+            <div className='bottom'>
+                <p className='temperature'>{Math.round(data.main.temp)}°C</p>
+                <div className='details'>
+                    <div className='parametar-row'>
+                        <span className='parametar-label'>Details</span>
+                    </div>
+                    <div className='parametar-row'>
+                        <span className='parametar-label'>Feels like</span>
+                        <span className='parametar-value'>{Math.round(data.main.feels_like)} °C</span>
+                    </div>
+                    <div className='parametar-row'>
+                        <span className='parametar-label'>Wind</span>
+                        <span className='parametar-value'>{data.wind.speed} m/s</span>
+                    </div>
+                    <div className='parametar-row'>
+                        <span className='parametar-label'>Humidity</span>
+                        <span className='parametar-value'>{data.main.humidity} %</span>
+                    </div>
+                    <div className='parametar-row'>
+                        <span className='parametar-label'>Pressure</span>
+                        <span className='parametar-value'>{data.main.pressure} Pa</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CurrentWeather;
